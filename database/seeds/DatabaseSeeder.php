3d8f5d6aea77f13894735124560f8c7381125f8e
Lina Brunken <linatt@hotmail.de>
2018-08-19 08:49:28 +0200

    repo mit paseto

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::createFromValues('John Doe', 'demo@demo.com', 'password');
+    }
+}
